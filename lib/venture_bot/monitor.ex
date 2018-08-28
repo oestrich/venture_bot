114fcb4e236baf3b1fb06f59fd6fbc24a93e92de
Eric Oestrich <eric@oestrich.org>
2018-08-28 19:21:59 -0400

    Allow for configurable kick off counts

--- lib/venture_bot/monitor.ex
+++ lib/venture_bot/monitor.ex
@@ -4,20 +4,28 @@ defmodule VentureBot.Monitor do
   require Logger
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, [])
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def kick_off(count \\ 100) do
+    GenServer.call(__MODULE__, {:kick_off, count})
   end
 
   def init(_) do
     Logger.info("Starting the monitor")
-    Process.send_after(self(), :kick_off, 1_000)
+    Process.send_after(self(), {:kick_off, 100}, 1_000)
     {:ok, %{}}
   end
 
-  def handle_info(:kick_off, state) do
+  def handle_cast({:kick_off, count}, state) do
+    handle_info({:kick_off, count}, state)
+  end
+
+  def handle_info({:kick_off, count}, state) do
     Logger.info("Kicking off clients")
 
-    Enum.each(1..1000, fn i ->
-      Process.send_after(self(), :start_child, i * 3000)
+    Enum.each(1..count, fn i ->
+      Process.send_after(self(), :start_child, i * 1000)
     end)
 
     {:noreply, state}
